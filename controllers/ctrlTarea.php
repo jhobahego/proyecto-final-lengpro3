5073fe1f796749c34feaa189be197c81766a39ab
Jhon Hernandez <jhobahego16@hotmail.com>
2023-02-27 15:04:20 -0500

    Creación del controlador de tarea

---
+++ controllers/ctrlTarea.php
@@ -0,0 +1,26 @@
+<?php
+    require_once '../models/mdlTarea.php';
+
+    if( !isset( $_POST['accion'] ) ){
+        echo 'No se determinó qué operación hacer.';
+    }
+
+    $objeto = new Tarea();
+
+    if( isset( $_POST['titulo'] ) ){
+        $objeto->setTitulo( $_POST['titulo'] );
+    }
+    if( isset( $_POST['nombre'] ) ){
+        $objeto->setDescripcion( $_POST['descripcion'] );
+    }
+
+    if( $_POST['accion'] == 'guardar' ){
+        $data = $objeto->guardar();
+
+        //$data = [ 'name' => 'God', 'age' => -1 ];
+        header('Content-type: application/json');
+        echo json_encode( $data );
+
+        //echo $respuesta;
+    }
+?>
